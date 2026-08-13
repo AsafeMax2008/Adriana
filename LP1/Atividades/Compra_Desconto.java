f0c49a81c31f4a15f796947e48fb3f2aea1f182d
AsafeMax2008 <maximianoasafe@gmail.com>
2026-08-12 22:55:47 -0300

    Add files via upload

---
+++ LP1/Atividades/Compra_Desconto.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Compra_Desconto {
+    void main(){
+        double SomaValores
+        int arroz;
+        int feijao;
+        int oleoSoja;
+        int acucar;
+
+
+        Scanner s=new Scanner(System.in);
+        IO.println("Digite o preço pago no arroz");
+        arroz = s. nextInt();
+        SomaValores=(SomaValores)+(arroz);
+        IO.println("Digite o preço pago no feijão");
+        feijao=s.nextInt();
+        SomaValores=(SomaValores)+(feijao)
+        IO.println("Digite o preço pago no óleo de soja");
+        SomaValores=(SomaValores)+(oleoSoja)
+        IO.println("Digite o preço pago no açúcar");
+        SomaValores=(SomaValores)+(acucar)
+
+
+
+    }
+}
